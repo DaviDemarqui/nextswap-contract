cc81e54164d0ee92d5dd917ccc612200aff5ca68
DaviDemarqui <davicoding@protonmail.com>
2024-04-06 00:53:26 -0300

    Installing toolbox

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 import '@openzeppelin/hardhat-upgrades';
+import "@nomicfoundation/hardhat-toolbox";
 
 module.exports = {
   solidity: "0.8.24",
