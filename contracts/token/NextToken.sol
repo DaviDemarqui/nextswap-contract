a8e09a66d0b8c3aa130e4bd28cfa1d8404e1ed07
DaviDemarqui <davicoding@protonmail.com>
2024-04-21 02:56:01 -0300

    Adding modifier to the token and some changes

--- contracts/token/NextToken.sol
+++ contracts/token/NextToken.sol
@@ -3,25 +3,30 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// author: @DaviDemarqui
+// @author: Dave
 // @notice: This is the LP Token contract for the Nextswap project.
-
 contract NextToken is ERC20 {
 
     address public owner;
     uint256 public _totalSupply;
 
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    modifier onlyOwner {
+        require(msg.sender == owner, "invalid sender");
+        _;
+    }
+
+    constructor(uint256 _initialSuppy) ERC20("NEXT Token", "NXT") {
+        _mint(msg.sender, _initialSuppy);
         owner = msg.sender;
     }
 
-    function mint(address to, uint256 amount) external {
+    function mint(address to, uint256 amount) onlyOwner external {
         require(msg.sender == owner, "Only the owner can mint");
         _mint(to, amount);
         _totalSupply += amount;
     }
 
-    function burn(address from, uint256 amount) external {
+    function burn(address from, uint256 amount) onlyOwner external {
         require(msg.sender == owner, "Only the owner can burn");
         _burn(from, amount);
         _totalSupply += amount;
