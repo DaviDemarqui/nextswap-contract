54f689e0155b39da7c844dc94808a7cf529881b2
DaviDemarqui <davicoding@protonmail.com>
2024-04-24 13:22:27 -0300

    Creating NextPoolFactory.sol

---
+++ contracts/NextPoolFactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// @author: Dave
+// @notice: This is the factory contract reponsable for creating
+// new NextPool contracts
+contract NextPoolFactory {
+    
+}
