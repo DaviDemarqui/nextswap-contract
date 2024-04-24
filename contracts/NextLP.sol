f1b25429a752790df42a9687a72561c77ad5b8ed
DaviDemarqui <davicoding@protonmail.com>
2024-04-24 13:22:46 -0300

    Creating NextLP.sol

---
+++ contracts/NextLP.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// @author: Dave
+// @notice: This is the Next Liquidity Provider
+// contract where the user interact to pools by
+// providing liquidity and earn fees
+contract NextLP {
+    
+}
