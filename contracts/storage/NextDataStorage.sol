7ab54bfb9295bfa8620930e43a9666736fb414fe
DaviDemarqui <davicoding@protonmail.com>
2024-04-25 17:48:24 -0300

    Creating a contract for global storage

---
+++ contracts/storage/NextDataStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// @author: Dave
+// @notice: This is the Next Data Storage, here global data
+// is stored in a organized and secure way to be used by 
+// any other contract in the project
+contract NextDataStorage {
+    
+}
